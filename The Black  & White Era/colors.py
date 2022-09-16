63c8dd943068c793a0acb3c6c2b8f8e316aa9599
Aditi Goyal <102579070+aditi5050@users.noreply.github.com>
2022-09-16 22:39:51 +0530

    Add files via upload

---
+++ The Black  & White Era/colors.py
@@ -0,0 +1,27 @@
+
+
+# packages
+import cv2
+
+# open image file
+image = cv2.imread('image.jpg', cv2.IMREAD_UNCHANGED)
+
+# convert image to grayscale
+image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# convert image to black and white
+thresh, image_black = cv2.threshold(image_grayscale, 170, 255, cv2.THRESH_BINARY)
+
+# display image in the window
+
+cv2.imshow('image.jpg', image_black)            # black and white image
+
+# store output images
+cv2.imwrite('image_grayscale.png', image_grayscale)
+cv2.imwrite('image_black.png', image_black)
+
+# break out on a key press
+cv2.waitKey(0)
+
+# clean up windows
+cv2.destroyAllWindows()
